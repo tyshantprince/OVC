4996f7620ab28ebe8fb449d120daec686dd8f855
tyshantprince <tyshantprince@gmail.com>
2019-08-22 08:03:18 -0700

    Report Page Updates

--- client/src/pages/Report.vue
+++ client/src/pages/Report.vue
@@ -1,7 +1,7 @@
 /* eslint-disable */
 <template>
-  <q-page class="q-pa-xl">
-    <div class="row justify-center">
+  <q-page class="q-pa-lg">
+    <div class="row justify-center ">
       <div class="col-12 text-center">
         <q-banner class style="background-color: #21c2b6; color: #fff">
           <h5 style="color: #fff">
@@ -10,95 +10,109 @@
               class="text-bold text-h4"
             >{{new Intl.ListFormat().format(categories.map((category) => { return category == "VictimRights" ? "Victim Rights" : category })) }}</span>
 
-            <br />Here is some information that might help address your needs.
+            <br />
+            <h5 class="q-pa-sm"> Here is some information that might help address your needs.</h5>
           </h5>
         </q-banner>
       </div>
+    </div>
 
-      <!-- Expansion Items -->
-      <div class="q-pa-sm" style="width: 80%">
-        <q-list bordered>
-          <q-expansion-item
-            :value="expanded == category"
-            @click="() => { expanded = category}"
-            class="q-pa-sm"
-            v-for="category in categories"
-            :key="category"
-            :group="category"
-            expand-separator
-            header-class="text-teal bg-white text-h4 text-center"
-          >
-            <template v-slot:header>
-         
+      <div class="row">
+        <div class="col-2">
+          <div class="column">
+            <div class="col text-center q-pa-lg" v-for="category in categories" :key="category">
+              <q-btn color="teal" align="center" glossy    @click="() => { expanded = category}">
+                <q-icon size="5em" :name="icons[category]" />
+              </q-btn>
+            </div>
+          </div>
+        </div>
 
-              <q-item-section>{{ category }}</q-item-section>
-            </template>
-            <q-card flat>
-              <q-card-section>
-                <p class="text-center">{{ UserReport[category].about}}</p>
-              </q-card-section>
+        <div class="col-10" style="min-width: 80%">
+          <!-- Expansion Items -->
+          <div class="q-pa-sm" style="min-width: 100%">
+            <q-list bordered>
+              <q-expansion-item
+              style="width: 100%"
+                :value="expanded == category"
+                @click="() => { expanded = category}"
+                class="q-pa-sm"
+                v-for="category in categories"
+                :key="category"
+                :group="category"
+                expand-separator
+                header-class="text-teal bg-white text-h4 text-center"
+              >
+                <template v-slot:header>
+                  <q-item-section>{{ category }}</q-item-section>
+                </template>
+                <q-card flat>
+                  <q-card-section>
+                    <p class="text-center" style="" >{{ UserReport[category].about}}</p>
+                  </q-card-section>
 
-              <q-card-section>
-                <div class="row">
-                  <div class="col q-pa-md">
-                    <h5>Next Steps</h5>
-                    <ul>
-                      <li v-for="(data, id) in UserReport[category].NextSteps" :key="id">
-                        <span v-if="data.includes('https')">
-                          <q-btn
-                          @click="openUrl(data.substring(data.indexOf('h'), data.lastIndexOf('/')))"
-                            type="a"
-                            color="teal"
-                            no-caps
-                          >Visit {{ data.substring(data.indexOf("/") + 2, data.indexOf("."))}}</q-btn>
-                        </span>
+                  <q-card-section>
+                    <div class="row">
+                      <div class="col q-pa-md">
+                        <h5>Next Steps</h5>
+                        <ul>
+                          <li v-for="(data, id) in UserReport[category].NextSteps" :key="id">
+                            <span v-if="data.includes('https')">
+                              <q-btn
+                                @click="openUrl(data.substring(data.indexOf('h'), data.lastIndexOf('/')))"
+                                type="a"
+                                color="teal"
+                                no-caps
+                              >Visit {{ data.substring(data.indexOf("/") + 2, data.indexOf("."))}}</q-btn>
+                            </span>
 
-                        <span v-else>{{ data }}</span>
-                      </li>
-                    </ul>
-                  </div>
-                  <div class="col q-pa-md">
-                    <h5>Find Help</h5>
-                    <ul>
-                      <li v-for="(data, id) in UserReport[category].FindHelp" :key="id">
-                        <span v-if="data.includes('https')">
-                          <q-btn
-                           @click="openUrl(data.substring(data.indexOf('h'), data.lastIndexOf('/')))"
-                            type="a"
-                            color="teal"
-                            no-caps
-                          >Visit {{ data.substring(data.indexOf("/") + 2, data.indexOf("."))}}</q-btn>
-                        </span>
+                            <span v-else>{{ data }}</span>
+                          </li>
+                        </ul>
+                      </div>
+                      <div class="col q-pa-md">
+                        <h5>Find Help</h5>
+                        <ul>
+                          <li v-for="(data, id) in UserReport[category].FindHelp" :key="id">
+                            <span v-if="data.includes('https')">
+                              <q-btn
+                                @click="openUrl(data.substring(data.indexOf('h'), data.lastIndexOf('/')))"
+                                type="a"
+                                color="teal"
+                                no-caps
+                              >Visit {{ data.substring(data.indexOf("/") + 2, data.indexOf("."))}}</q-btn>
+                            </span>
 
-                        <span v-else>{{ data }}</span>
-                      </li>
-                    </ul>
-                  </div>
-                  <div class="col q-pa-md">
-                    <h5>More Info</h5>
-                    <ul>
-                      <li v-for="(data, id) in UserReport[category].MoreInfo" :key="id">
-                        <span v-if="data.includes('https')">
-                          <q-btn
-                           @click="openUrl(data.substring(data.indexOf('h'), data.lastIndexOf('/')))"
-                            type="a"
-                            color="teal"
-                            no-caps
-                          >Visit {{ data.substring(data.indexOf("/") + 2, data.indexOf("."))}}</q-btn>
-                        </span>
+                            <span v-else>{{ data }}</span>
+                          </li>
+                        </ul>
+                      </div>
+                      <div class="col q-pa-md">
+                        <h5>More Info</h5>
+                        <ul>
+                          <li v-for="(data, id) in UserReport[category].MoreInfo" :key="id">
+                            <span v-if="data.includes('https')">
+                              <q-btn
+                                @click="openUrl(data.substring(data.indexOf('h'), data.lastIndexOf('/')))"
+                                type="a"
+                                color="teal"
+                                no-caps
+                              >Visit {{ data.substring(data.indexOf("/") + 2, data.indexOf("."))}}</q-btn>
+                            </span>
 
-                        <span v-else>{{ data }}</span>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </q-card-section>
-            </q-card>
-          </q-expansion-item>
+                            <span v-else>{{ data }}</span>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </q-card-section>
+                </q-card>
+              </q-expansion-item>
 
-          <q-separator />
-        </q-list>
-      </div>
+              <q-separator />
+            </q-list>
+          </div>
+        </div>
     </div>
   </q-page>
 </template>
@@ -126,9 +140,8 @@ export default {
     };
   },
   methods: {
-    openUrl(url){
+    openUrl(url) {
       window.open(url, "_blank");
-
     },
     handleIconClick(name) {
       // var container = this.$el.querySelector("#" + name);
